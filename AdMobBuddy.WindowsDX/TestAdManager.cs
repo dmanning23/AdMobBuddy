346804414b018981c7d02c8ad3b16c94f28417b0
Dan Manning <dmanning23@hotmail.com>
2018-10-06 18:00:13 -0500

    Added stub windows project

---
+++ AdMobBuddy.WindowsDX/TestAdManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace AdMobBuddy.WindowsDX
+{
+	/// <summary>
+	/// This is a stubbed out AdManager class for testing your app on WindowsDX
+	/// </summary>
+	public class TestAdManager : IAdManager
+	{
+		public event EventHandler<RewardedVideoEventArgs> OnVideoReward;
+
+		public virtual void DisplayInterstitialAd()
+		{
+		}
+
+		public virtual void DisplayRewardedVideoAd()
+		{
+			VideoReward(this, new RewardedVideoEventArgs(true));
+		}	
+
+		protected void VideoReward(object obj, RewardedVideoEventArgs e)
+		{
+			if (null != OnVideoReward)
+			{
+				OnVideoReward(obj, e);
+			}
+		}
+	}
+}
